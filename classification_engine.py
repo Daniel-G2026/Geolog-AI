092dd5f50aa43006b6fced30ac96964f28f267b2
Daniel Anazodo <danyanazodo@gmail.com>
2026-03-21 13:52:21 -0400

    add classification engine with N-value to consistency/density logic

---
+++ classification_engine.py
@@ -0,0 +1,52 @@
+COHESIVE_TERMS = ["clay", "silty clay", "clayey silt","silt"]
+COHESIONLESS_TERMS = ["sand", "gravel", "silty sand", "sandy silt", "sand and silt"]
+
+
+def get_consistency_density(soil_name: str,n_value: int):
+    soil_lower = soil_name.lower()
+    if soil_lower.endswith("till"):
+        soil_lower = soil_lower.replace("till","")
+    if any(term in soil_lower for term in COHESIONLESS_TERMS ):
+        if 0 <= n_value < 4:
+            return "very loose"
+        elif n_value == 4:
+            return "very loose to loose"
+        elif 4 < n_value < 10:
+            return "loose"
+        elif n_value == 10:
+            return "loose to medium dense"
+        elif 10 < n_value < 30:
+            return "medium dense"
+        elif n_value == 30:
+            return "medium dense to dense"
+        elif 30 < n_value < 50:
+            return "dense"
+        elif n_value == 50:
+            return "dense to very dense"
+        elif n_value > 50:
+            return "very dense"
+    elif any(term in soil_lower for term in COHESIVE_TERMS):
+        if 0 <= n_value < 2:
+            return "very soft"
+        elif n_value == 2:
+            return "very soft to soft"
+        elif 2 < n_value < 4:
+            return "soft"
+        elif n_value == 4:
+            return "soft to firm"
+        elif 4 < n_value < 8:
+            return "firm"
+        elif n_value == 8:
+            return "firm to stiff"
+        elif 8 < n_value < 15:
+            return "stiff"
+        elif n_value == 15:
+            return "stiff to very stiff"
+        elif 15 < n_value < 30:
+            return "very stiff"
+        elif n_value ==30:
+            return "very stiff to hard"
+        elif n_value > 30:
+            return "hard"
+    else:
+        return None 
