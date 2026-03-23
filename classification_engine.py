4710bee48e04e20f7d49ff71146cbd68ec5664dc
Daniel Anazodo <danyanazodo@gmail.com>
2026-03-22 23:32:14 -0400

    add parse_blow_counts with all SPT refusal cases

--- classification_engine.py
+++ classification_engine.py
@@ -1,6 +1,6 @@
 COHESIVE_TERMS = ["silty clay", "clayey silt","silt","clay"]
 COHESIONLESS_TERMS = ["sand and silt", "silty sand", "sandy silt", "gravel","sand"]
-
+INCHES_TO_MM = {1: 25, 2: 51, 3: 76, 4: 102, 5: 127, 6: 152}
 
 def get_consistency_density(soil_name: str,n_value: int):
     soil_lower = soil_name.lower()
@@ -50,3 +50,32 @@ def get_consistency_density(soil_name: str,n_value: int):
             return "hard"
     else:
         return None 
+    
+def parse_blow_counts(blow_counts: list, pen_depths: list) -> dict:
+    if len(blow_counts) == 4:
+        n_value = blow_counts[1] + blow_counts[2]
+        n_value_log = str(n_value)
+        refusal = False
+    elif len(blow_counts) == 1:
+        n_value = blow_counts[0]
+        pen_depth = round(pen_depths[0] * 25.4)
+        n_value_log = f"{blow_counts[0]}/{pen_depth}mm"
+        refusal = True
+    elif len(blow_counts) == 2:
+        n_value = blow_counts[1]
+        pen_depth = round(pen_depths[1] * 25.4)
+        n_value_log = f"{blow_counts[1]}/{pen_depth}mm"
+        refusal = True
+    else:
+        n_value = blow_counts[1] + blow_counts[2]
+        pen_depth = round((pen_depths[1] + pen_depths[2]) * 25.4)
+        n_value_log = f"{n_value}/{pen_depth}mm"
+        refusal = True
+
+
+    return {
+            "n_value": n_value,
+            "n_value_log": n_value_log,
+            "refusal": refusal
+            }   
+        
